0bd33c0a3d8b4eee48c03aea40c564125d7f0661
ky5burke <ky5burke@gmail.com>
2024-02-09 11:45:31 -0600

    working with javascript arithmetic functions

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2
+const num2 = 31
+const num3 = 29
+const num4 = 5
 
-// Write your code here
+const multiply = num1 * num2
+
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const mod = num3 % num4
+
+const set = [5, 15, 10, 20]
+const max = Math.max(5, 10, 15, 20)
